b5b2903c8f31d6108a78420b245c4e3e5cc98763
Camila Jullyane <camilaj.silva0@gmail.com>
2024-03-30 20:35:26 -0300

    update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,6 +6,7 @@ const chat = document.querySelector(".chat")
 const chatForm = document.querySelector(".chat_form");
 const chatInput = document.querySelector(".chat_input");
 
+const chatmessages = document.querySelector(".chat_messages");
 
 const user = {id: "", name: "", color: ""}
 
@@ -17,8 +18,38 @@ function randomColor() {
     return colors[i];
 }
 
+
+const createMessageSelf = (content) => {
+    const div = document.createElement("div");
+    div.classList.add("message_self");
+    div.innerHTML = content;
+
+    return div;
+} 
+
+
+const createMessageOther = (content, sender, senderColor) => {
+    const div = document.createElement("div");
+    const span = document.createElementl("span")
+    div.classList.add("message_self");
+    div.innerHTML = content;
+
+    return div;
+} 
+
+
+const processMessage = ({data}) => {
+    const {userId, userName, userColor, content} = JSON.parse(data);
+
+    const element = createMessageSelf(content);
+
+    chatmessages.appendChild(element);
+
+}
+
+
 let websocket;
-const handleSubmit = (event) => {
+const handleLogin = (event) => {
 
     event.preventDefault();
     user.id = crypto.randomUUID(); 
@@ -29,10 +60,23 @@ const handleSubmit = (event) => {
     chat.style.display = "flex";
 
     websocket = new WebSocket("ws://localhost:8080");
-    websocket.onopen = () => websocket.send(`Usuário ${user.name} entrou no chat`);
-    
-    console.log(user);
+    websocket.onmessage = processMessage    
 }
 
 
-loginForm.addEventListener("submit", handleSubmit);
+const sendMessage = (event) => {
+    event.preventDefault();
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+
+    websocket.send(JSON.stringify(message));
+    chatInput.value = "";
+}
+
+
+loginForm.addEventListener("submit", handleLogin);
+chatForm.addEventListener("submit", sendMessage);
